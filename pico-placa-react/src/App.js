dc635b8ca75b8c2321820556c6466b14520d25ac
MNNoboa <manoboa97@gmail.com>
2021-03-04 02:07:49 -0500

    Date validation correction and date parsing

--- pico-placa-react/src/App.js
+++ pico-placa-react/src/App.js
@@ -3,11 +3,14 @@ import './App.css';
 import { useReducer } from 'react';
 
 const regExDict = {
-  pn_RegEx: /^([a-zA-Z]{3}|[a-zA-Z]{0})\d{3,4}$/,
-  date_RegEx: /^\d{2}[-/]{1}\d{2}($|[-/]{1}(\d{2}|\d{4})$)/,
-  date_separator_RegEx: /[-/]/g,
-  invalid_RegEx: /(?=[^a-zA-Z])(?=\D)./g
+  pn_RegEx: /^([a-zA-Z]{3}\d{3,4}$|^\d{3,4}$)/,
+  shortdate_RegEx: /^\d{2}[-/]{1}\d{2}$/,
+  longdate_RegEx: /^\d{2}[-/]{1}\d{2}[-/]{1}(\d{2}|\d{4})$/,
+  invalid_pn_RegEx: /(?=[^a-zA-Z])(?=\D)./g,
+  invalid_date_RegEx: /(?=[^a-zA-Z/-])(?=\D)./g
 }
+
+const initDateTime = new Date();
 
 function App() {
 
@@ -16,6 +19,7 @@ function App() {
       plate_number: '',
       date: '',
       time: '',
+      DateTime: initDateTime,
       pn_valid: false,
       date_valid: false,
       time_valid: false,
@@ -25,26 +29,38 @@ function App() {
   
   const [state, dispatch] = useReducer(reducer, {}, init)
 
-  function cleanInput(item){
-    return item.replace(regExDict.invalid_RegEx,'');
+  function cleanInput(item,regex){
+    return item.replace(regex,'');
   }
 
   function reducer(state, action){
     switch(action.type){
       case 'pn_change':
-        let pn = cleanInput(action.payload);
+        const pn = cleanInput(action.payload,regExDict.invalid_pn_RegEx);
         return {pn_valid:regExDict.pn_RegEx.test(pn),plate_number:pn}
-      case 'date_change':
-        let d = cleanInput(action.payload);
-        let dv = regExDict.date_RegEx.test(d);
-        return {date_valid:dv,date:d}
-        // if (!dv)
-        //   return {date_valid:dv, date:d};
-        // let date_arr = d.split(regExDict.date_separator_RegEx);
-        // if(date_arr.length>2)
-        // let d1 = parseInt(date_arr[0]);
+      case 'date_change':        
+        const d = cleanInput(action.payload,regExDict.invalid_date_RegEx);
+        let dateTime = new Date(d);
+
+        if (!isNaN(dateTime.getDay()))
+          return {DateTime:dateTime,date_valid:true,date:d};
+        
+        if(regExDict.shortdate_RegEx.test(d)){
+          let d_alter = d.substring(3,5) + '/' + d.substring(0,2);
+          dateTime = new Date(d_alter);
+          dateTime.setFullYear(initDateTime.getFullYear());
+          return {DateTime:dateTime,date_valid:isNaN(dateTime.getDay()),date:d};
+        }
+        if(regExDict.longdate_RegEx.test(d)){
+          let d_alter = d.substring(3,5) + '/' + d.substring(0,2) + '/' + d.substring(6,10);
+          dateTime = new Date(d_alter);
+          return {DateTime:dateTime,date_valid:isNaN(dateTime.getDay()),date:d};
+        }
+        return {DateTime:initDateTime, date_valid:false,date:d}
+                
       case 'time_change':
-        console.log(typeof action.payload,'['+action.payload+']');
+
+        // console.log(typeof action.payload,'['+action.payload+']');
         return {time_valid:true};
       default:
         throw new Error();
@@ -68,12 +84,17 @@ function App() {
         <label>
           Date: <input className="App-input" type="text" value={state.date} 
           onChange={(e)=>{dispatch({type:'date_change', payload: e.target.value})}} 
-          placeholder='dd/MM/YYYY'/>
+          placeholder='MM/dd/YYYY'/>
         </label>
         <label>
           Time: <input className="App-input" type="time" value={state.time} 
           onChange={(e)=>{dispatch({type:'time_change', payload: e.target.value})}} />
         </label>
+        {state.DateTime &&
+          <div>
+            <p>{state.DateTime.toString()}</p>
+          </div>
+        }
       </div>
       
       <footer>
