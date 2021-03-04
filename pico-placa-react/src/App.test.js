3ffcb713882657a906aa190b27b58405b5243a7e
MNNoboa <manoboa97@gmail.com>
2021-03-04 09:09:26 -0500

    Final tests and cleanup

--- pico-placa-react/src/App.test.js
+++ pico-placa-react/src/App.test.js
@@ -1,8 +1,43 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
+import { unmountComponentAtNode } from 'react-dom';
 import App from './App';
 
-test('renders learn react link', () => {
+let container = null;
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+test('renders without crashing', () =>{
+  const div = document.createElement('div');
+  render(<App />, div);
+  unmountComponentAtNode(div);
+})
+
+test('renders app and datetime div', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const dateElement = screen.getByText(/date time:/i);
+  expect(dateElement).toBeInTheDocument();
+});
+
+test('writes on plate number input and checks warning', ()=>{
+  render(<App/>);
+  fireEvent.change(screen.getByPlaceholderText(/plate numbers/i),{target:{value:'aaaa'}});
+  const warningElement = screen.getByText(/please enter a correct plate/i);
+  expect(warningElement).toBeInTheDocument();
+});
+
+test('writes on date input and checks warning', ()=>{
+  render(<App/>);
+  fireEvent.change(screen.getByPlaceholderText(/[MM/dd/YYYY]/),{target:{value:'baacs-a'}});
+  const warningElement = screen.getByText(/please enter a correct date/i);
+  expect(warningElement).toBeInTheDocument();
 });
